8dc418f06a8f94a7c28e480554b61af457e1910f
Tommy Chan <ckchan.cs@gmail.com>
2015-06-15 18:30:39 +0800

    Perform brew update and cleanup before install

--- install.sh
+++ install.sh
@@ -6,9 +6,14 @@ function removeHomebrew() {
     which -s brew && echo 'Pre-installed Homebrew still exists...' && exit 1 || (echo 'Pre-installed Homebrew uninstalled!' && exit 0)
 }
 
-function installTools() {
+function installHomebrewAndCask() {
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew update
+    brew cleanup
     brew install caskroom/cask/brew-cask
+
+    # Set application symlinks to /Applications
+    (echo && echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"') >> ~/.bash_profile
 }
 
-removeHomebrew || installBasic;
+installHomebrewAndCask
